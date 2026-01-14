5e729ae55ff5044750ca1c3850c3982d3cd55440
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2026-01-14 07:00:09 -0700

    feat: Add Game component and change export default to Game instead of Board, create handlePlay func within game. update Board by removing setStates and replacing with onPlay, add props xIsNext/squares/onPlay to Board

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-export default function Board() {
+function Board({ xIsNext, squares, onPlay }) {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
@@ -24,8 +24,7 @@ export default function Board() {
     } else {
       nextSquares[i] = "O";
     }
-    setSquares(nextSquares); // key logic here to update state
-    setXIsNext(!xIsNext); // flips the state of xIsNext to opposite it's current state/value
+    onPlay(nextSquares);
   }
   const winner = calculateWinner(squares);
   let status;
@@ -54,6 +53,26 @@ export default function Board() {
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
+  );
+}
+
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares])
+    setXIsNext(!xIsNext)
+  }
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
   );
 }
 
